7b520c9714d759b38e2fb1f017db273a565438f9
Subham-SR <sarkarsubham747@gmail.com>
2025-06-16 12:30:47 +0530

    Add robofriends

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id:1,
+        name:'Leanne Graham',
+        username: 'Breat',
+        email: 'sincere@april.big'
+    },
+    {
+        id:2,
+        name:'Elcvish Neuton',
+        username: 'Teneson',
+        email: 'maliowsious@may.big'
+    },
+    {
+        id:3,
+        name:'Leanne Graham',
+        username: 'Breat',
+        email: 'sincere@april.big'
+    },
+    {
+        id:4,
+        name:'Leanne Graham',
+        username: 'Breat',
+        email: 'sincere@april.big'
+    },
+    {
+        id:5,
+        name:'Leanne Graham',
+        username: 'Breat',
+        email: 'sincere@april.big'
+    },
+    {
+        id:6,
+        name:'Leanne Graham',
+        username: 'Breat',
+        email: 'sincere@april.big'
+    },
+    {
+        id:7,
+        name:'Leanne Graham',
+        username: 'Breat',
+        email: 'sincere@april.big'
+    }
+];
